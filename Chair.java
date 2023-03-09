3c89a1c920ef56c97b16af1492d4a0e735856b5c
Denys <116681720+d3nnyyy@users.noreply.github.com>
2023-03-09 17:13:08 +0200

    adding my lab with the description of a chair

---
+++ Chair.java
@@ -0,0 +1,79 @@
+package ua.lviv.iot.algo.part1.lab1;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Chair {
+
+    private int id = 1;
+    private int maxWeight;
+    private String material;
+    private String owner;
+
+    private static Chair instance;
+
+
+    public static Chair getInstance(){
+        if(instance == null){
+            instance = new Chair();
+        }
+        return instance;
+    }
+
+    @Override
+    public String toString() {
+        return "Chair{" +
+                "id=" + id +
+                ", maxWeight=" + maxWeight +
+                ", material='" + material + '\'' +
+                ", owner='" + owner + '\'' +
+                '}';
+    }
+
+    public void occupy(){
+        System.out.println("This chair is currently occupied by " + this.owner);
+    }
+
+    public void release(){
+        this.owner = null;
+    }
+
+    public boolean isOccupied(){
+        return (this.owner != null);
+    }
+
+    public static void main(String[] args) {
+        Chair[] chairs = new Chair[5];
+        chairs[0] = new Chair();
+        chairs[1] = new Chair(1,200, "wood", "Petya");
+        chairs[2] = new Chair(2,250, "metal", "Vasya");
+
+        Chair chair1 = Chair.getInstance();
+        chair1.setId(3);
+        chair1.setMaxWeight(100);
+        chair1.setMaterial("plastic");
+        chair1.setOwner(null);
+
+        Chair chair2 = Chair.getInstance();
+        chair2.setId(4);
+        chair2.setMaxWeight(250);
+        chair2.setMaterial("leather");
+        chair2.setOwner(null);
+
+        chairs[3] = chair1;
+        chairs[4] = chair2;
+
+        for (Chair chair : chairs){
+            System.out.println(chair.toString());
+        };
+
+        System.out.println(chairs[1].getOwner());
+        System.out.println(chairs[2].isOccupied());
+
+    }
+
+}
