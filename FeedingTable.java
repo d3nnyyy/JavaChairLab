51a64aadb1ce39f481d8290b97243e8d484c1593
Denys <116681720+d3nnyyy@users.noreply.github.com>
2023-03-31 11:13:49 +0300

    fixed after review
    added and removed empty lines

--- FeedingTable.java
+++ FeedingTable.java
@@ -1,10 +1,9 @@
 package ua.lviv.iot.algo.part1.lab1;
-
 import lombok.Getter;
 
 @Getter
-
 public class FeedingTable extends Chair {
+
     private static double minHeight = 0.5;
     private static double maxHeight = 1;
     private double height;
